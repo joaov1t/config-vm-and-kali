b7bea69999988c9aa30993a89ba545c6b77c2e43
João Vitor <138710662+joaov1t@users.noreply.github.com>
2026-03-28 15:54:59 -0300

    Modify VM startup script with new placeholders
    Updated IP, user, and RSA path placeholders for clarity.

--- kali-start.bat
+++ kali-start.bat
@@ -1,18 +1,19 @@
 @echo off
 SET VM_NAME="kali-linux-2025.3-virtualbox-amd64"
 SET DIR_VM="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
-SET IP="Defina-seu-IP"
-SET USR="Defina-o-seu-user"
-SET RSA="Defina-o-caminho-da-rsa"
+SET IP="add-ip-do-kali"
+SET USR="add-o-seu-user"
+SET RSA="add-o-caminho-da-rsa"
 
 cls
 REM Inicia a máquina virtual
 echo Iniciando a maquina virtual Kali Linux...
 %DIR_VM% startvm "%VM_NAME%" --type gui
 
-REM Aguarda 60 segundos
-echo Aguardando 60 segundos para inicializacao...
-timeout /t 60 /nobreak 
+# Voce pode mexer de acordo com o tempo que seu pc demora para inicar a vm
+REM Aguarda 130 segundos
+echo Aguardando 130 segundos para inicializacao...
+timeout /t 130 /nobreak 
 
 REM Conecta via SSH
 ssh -i %RSA% %USR%@%IP%
