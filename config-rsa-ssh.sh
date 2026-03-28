85076f66e95f22ce1254c09609b85ca62ca74486
João Vitor <138710662+joaov1t@users.noreply.github.com>
2026-03-28 16:00:06 -0300

    Add script to generate and configure SSH key

---
+++ config-rsa-ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "[+] Gerando nova chave SSH..."
+
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa
+
+echo "[+] Adicionando chave pública ao authorized_keys..."
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+
+echo "[+] Chave configurada!"
+
+echo "⚠️ Sua chave privada está em: ~/.ssh/id_rsa"
